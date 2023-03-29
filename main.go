3759b551dd7a53f31c1f5cda5bd53b591da8f274
Matheus Lino <matheuslino86@gmail.com.br>
2023-03-29 09:40:26 -0400

    feat: :sparkles: Count requests

--- main.go
+++ main.go
@@ -17,15 +17,28 @@ var onlineUsers = prometheus.NewGauge(prometheus.GaugeOpts{
 	},
 })
 
+var httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "goapp_http_requests_total",
+	Help: "Count of all HTTP requests for goapp",
+}, []string{})
+
 func main() {
 	r := prometheus.NewRegistry()
 	r.MustRegister(onlineUsers)
+	r.MustRegister(httpRequestsTotal)
 
 	go func() {
 		for {
 			onlineUsers.Set(float64(rand.Intn(2000)))
 		}
 	}()
+
+	home := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello Full Cycle"))
+	})
+
+	http.Handle("/", promhttp.InstrumentHandlerCounter(httpRequestsTotal, home))
 
 	http.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
 	log.Fatal(http.ListenAndServe(":8181", nil))
